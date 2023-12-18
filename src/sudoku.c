fc148696b0d60c23a56079cb3933c2ee0a68f4b3
faugeu <121447325+faugeu@users.noreply.github.com>
2023-12-18 14:44:20 +0000

    Remove redundant "continue"

--- src/sudoku.c
+++ src/sudoku.c
@@ -8,6 +8,7 @@
 #include "naked_pairs.h"
 #include "naked_triples.h"
 #include "hidden_pairs.h"
+#include "hidden_triples.h"
 
 int main(int argc, char **argv)
 {
@@ -42,19 +43,21 @@ int main(int argc, char **argv)
         if (solved_counter)
         {
             printf("naked_pairs %d\n", solved_counter);
-            continue;
-        }
-        solved_counter = hidden_pairs(board);
-        if (solved_counter)
-        {
-            printf("hidden_pairs %d\n", solved_counter);
-            continue;
         }
         solved_counter = naked_triples(board);
         if (solved_counter)
         {
             printf("naked_tripples %d\n", solved_counter);
-            continue;
+        }
+        solved_counter = hidden_pairs(board);
+        if (solved_counter)
+        {
+            printf("hidden_pairs %d\n", solved_counter);
+        }
+        solved_counter = hidden_triples(board);
+        if (solved_counter)
+        {
+            printf("hidden_tripples %d\n", solved_counter);
         }
     }
     print_solution(board);
